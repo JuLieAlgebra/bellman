e57e8ae5394029cc92fc2b939ae2933bb96a6468
VeggieBaconator <julieannabacon2014@gmail.com>
2020-03-25 23:49:49 -0400

    Work on gridboi dynamics and reward methods

--- source/gridboi.cpp
+++ source/gridboi.cpp
@@ -20,6 +20,12 @@ public:
         struct Coord {
             uint x;
             uint y;
+            bool operator==(Coord const& other) const {
+                return (x == other.x) and (y == other.y);
+            }
+            bool operator!=(Coord const& other) const {
+                return (x != other.x) or (y != other.y);
+            }
         };
         Coord boi;
         Coord gob;
@@ -59,12 +65,48 @@ public:
         return s;
     }
 
-    Real dynamic(Index s, Index a, Index s1) const {
-        return 0; // ???
+    // Returns the probability of transitioning to state s1 given state s and action a
+    Real dynamic(Index s_index, Index a, Index s1_index) const {
+        State const s = state_from_index(s_index);
+        State const s1 = state_from_index(s1_index);
+        // Evaluate validity of boi move
+        if(a == Action::WAIT) {
+            // Stand still
+            if(s1.boi != s.boi) return 0.0;
+        } else if(a == Action::UP) {
+            // Stand still if top of grid
+            if((s.boi.y == nY-1) and (s1.boi != s.boi)) return 0.0;
+            // Move y+1
+            else if((s1.boi.x != s.boi.x) or (s1.boi.y != s.boi.y+1)) return 0.0;
+        } else if(a == Action::DOWN) {
+            // Stand still if bottom of grid
+            if((s.boi.y == 0) and (s1.boi != s.boi)) return 0.0;
+            // Move y-1
+            else if((s1.boi.x != s.boi.x) or (s1.boi.y != s.boi.y-1)) return 0.0;
+        } else if(a == Action::LEFT) {
+            // Stand still if leftmost of grid
+            if((s.boi.x == 0) and (s1.boi != s.boi)) return 0.0;
+            // Move x-1
+            else if((s1.boi.x != s.boi.x-1) or (s1.boi.y != s.boi.y)) return 0.0;
+        } else if(a == Action::RIGHT) {
+            // Stand still if rightmost of grid
+            if((s.boi.x == nX-1) and (s1.boi != s.boi)) return 0.0;
+            // Move x+1
+            else if((s1.boi.x != s.boi.x+1) or (s1.boi.y != s.boi.y)) return 0.0;
+        }
+        // TODO ???
+        Real p = 1.0;
+        return p;
     }
 
-    Real reward(Index s, Index a) const {
-        return 0; /// ???
+    // Returns the (deterministic) reward for selecting action a in state s
+    Real reward(Index s_index, Index a) const {
+        State const s = state_from_index(s_index);
+        // Get the goo!
+        if(s.boi == s.goo) return 1;
+        // Avoid the gob!
+        if(s.boi == s.gob) return -1;
+        return 0;
     }
 };
 
